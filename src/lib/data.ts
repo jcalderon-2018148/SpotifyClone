a6b040d182bbd7857a593b0fa4e1b9cf24e5cd99
jcalderon-2018148 <jcalderon-2018148@kinal.edu.gt>
2024-07-14 16:32:30 -0600

    Fixes

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -13,47 +13,47 @@ export const playlists: Playlist[] = [
   {
     id: 'Date a Live',
     albumId: 1,
-    title: "Chill Lo-Fi Music",
+    title: "SweetARMS Playlist",
     color: colors.yellow,
     cover:
-      "https://vinyl.lofirecords.com/cdn/shop/products/VINYL_MORNING_COFFEE_4-min.png?v=1680526353",
-    artists: ["NoSpirit", "Casiio"],
+      "https://lh3.googleusercontent.com/upphfctNzzBp0hA6JMhcD1hGmdirzuAzVJQEri4YGqREmBbvU2M4v1kaeO9SLbHwozrrDPxdiB1lDkA=w544-h544-l90-rj",
+    artists: ["SweetARMS", "Casiio"],
   },
   {
     id: 'Oregairu',
     albumId: 2,
-    title: "Lo-Fi Chill Session",
+    title: "Yanaginagi Playlist",
     color: colors.green,
     cover:
-      "https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187",
-    artists: ["Kupla", "Blue Fox"],
+      "https://lh3.googleusercontent.com/zO1w1AewNSdvuJxmmdpY87W2bHi-qPPBiXBezbvm61IcdPWx57RFlfFyjEQeqesoRKf3_-34y_hu2KfY=w544-h544-s-l90-rj",
+    artists: ["Yanaginagi", "Blue Fox"],
   },
   {
     id: 'Re Zero',
     albumId: 3,
-    title: "Study Session",
+    title: "Memento Playlist",
     color: colors.rose,
     cover:
-      "https://f4.bcbits.com/img/a1435058381_65.jpg",
-    artists: ["Tenno", "xander", "Team Astro"],
+      "https://lh3.googleusercontent.com/cP82wBv04ib5r_7V_8lAgCXLhHTMztEKcDz6UWHHw3QuuPVKDsW68VesTgyYxSEVx9Fnb01PoFl4iD2RPQ=w544-h544-l90-rj",
+    artists: ["nonoc", "Team Astro"],
   },
   {
     id: 'Soul Eater',
     albumId: 4,
-    title: "Blue Note Study Time",
+    title: "Absolute Soul Playlist",
     color: colors.blue,
     cover:
-      "https://f4.bcbits.com/img/a1962013209_16.jpg",
-    artists: ["Raimu", "Yasumu"],
+      "https://lh3.googleusercontent.com/WSUuoJZ2CwEkT7udiZzZyhGBe2jbiU2QKq894Nc7ZkhDTv0KL1odxJPeuRsFSS6Wi1BP7kWOTYLFOdSDxA=w544-h544-s-l90-rj",
+    artists: ["Konomi Suzuki", "Yasumu"],
   },
   {
     id: 'TONIKAWA',
     albumId: 5,
-    title: "Chau Saura Session",
+    title: "Moonlit Walk Playlist",
     color: colors.purple,
     cover:
-      "https://f4.bcbits.com/img/a2793859494_16.jpg",
-    artists: ["Chau Saura", "amies", "kyu"],
+      "https://lh3.googleusercontent.com/zce5PzsKTKtZlRutG6CLbVb03US9DOiE7nosm1--_tOxyFCnu7GlmPMhzaVMm--lSnjYpaifhFJtY8A=w544-h544-l90-rj",
+    artists: ["Neko Hacker"],
   },
   {
     id: '6',
@@ -97,7 +97,7 @@ export const songs: Song[] = [
     "id": 1,
     "albumId": 1,
     "title": "I swear",
-    "image": `https://vinyl.lofirecords.com/cdn/shop/products/VINYL_MORNING_COFFEE_4-min.png?v=1680526353`,
+    "image": `https://lh3.googleusercontent.com/upphfctNzzBp0hA6JMhcD1hGmdirzuAzVJQEri4YGqREmBbvU2M4v1kaeO9SLbHwozrrDPxdiB1lDkA=w544-h544-l90-rj`,
     "artists": ["sweet ARMS"],
     "album": "Chill Lo-Fi Music",
     "duration": "3:12"
@@ -106,7 +106,7 @@ export const songs: Song[] = [
     "id": 2,
     "albumId": 1,
     "title": "Invisible Date",
-    "image": `https://vinyl.lofirecords.com/cdn/shop/products/VINYL_MORNING_COFFEE_4-min.png?v=1680526353`,
+    "image": `https://lh3.googleusercontent.com/upphfctNzzBp0hA6JMhcD1hGmdirzuAzVJQEri4YGqREmBbvU2M4v1kaeO9SLbHwozrrDPxdiB1lDkA=w544-h544-l90-rj`,
     "artists": ["sweet ARMS"],
     "album": "Chill Lo-Fi Music",
     "duration": "4:07"
@@ -142,7 +142,7 @@ export const songs: Song[] = [
     "id": 1,
     "albumId": 2,
     "title": "Silent Rain",
-    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "image": `https://lh3.googleusercontent.com/zO1w1AewNSdvuJxmmdpY87W2bHi-qPPBiXBezbvm61IcdPWx57RFlfFyjEQeqesoRKf3_-34y_hu2KfY=w544-h544-s-l90-rj`,
     "artists": ["Urban Nocturne"],
     "album": "Midnight Tales",
     "duration": "3:40"
@@ -151,7 +151,7 @@ export const songs: Song[] = [
     "id": 2,
     "albumId": 2,
     "title": "Lost Pages",
-    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "image": `https://lh3.googleusercontent.com/zO1w1AewNSdvuJxmmdpY87W2bHi-qPPBiXBezbvm61IcdPWx57RFlfFyjEQeqesoRKf3_-34y_hu2KfY=w544-h544-s-l90-rj`,
     "artists": ["Urban Nocturne"],
     "album": "Midnight Tales",
     "duration": "3:20"
@@ -160,7 +160,7 @@ export const songs: Song[] = [
     "id": 3,
     "albumId": 2,
     "title": "Midnight Tales",
-    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "image": `https://lh3.googleusercontent.com/zO1w1AewNSdvuJxmmdpY87W2bHi-qPPBiXBezbvm61IcdPWx57RFlfFyjEQeqesoRKf3_-34y_hu2KfY=w544-h544-s-l90-rj`,
     "artists": ["Urban Nocturne"],
     "album": "Midnight Tales",
     "duration": "3:50"
@@ -169,7 +169,7 @@ export const songs: Song[] = [
     "id": 4,
     "albumId": 2,
     "title": "City Lights",
-    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "image": `https://lh3.googleusercontent.com/zO1w1AewNSdvuJxmmdpY87W2bHi-qPPBiXBezbvm61IcdPWx57RFlfFyjEQeqesoRKf3_-34y_hu2KfY=w544-h544-s-l90-rj`,
     "artists": ["Urban Nocturne"],
     "album": "Midnight Tales",
     "duration": "3:30"
@@ -178,7 +178,7 @@ export const songs: Song[] = [
     "id": 5,
     "albumId": 2,
     "title": "Night Drive",
-    "image": `https://vinyl.lofirecords.com/cdn/shop/files/2amsynth-vinyl.png?v=1693312187`,
+    "image": `https://lh3.googleusercontent.com/zO1w1AewNSdvuJxmmdpY87W2bHi-qPPBiXBezbvm61IcdPWx57RFlfFyjEQeqesoRKf3_-34y_hu2KfY=w544-h544-s-l90-rj`,
     "artists": ["Urban Nocturne"],
     "album": "Midnight Tales",
     "duration": "4:20"
@@ -186,11 +186,11 @@ export const songs: Song[] = [
   {
     "id": 1,
     "albumId": 3,
-    "title": "Lunar",
-    "image": `https://f4.bcbits.com/img/a1435058381_65.jpg`,
-    "artists": ["Tenno"],
-    "album": "Study Session",
-    "duration": "3:40"
+    "title": "Memento",
+    "image": `https://lh3.googleusercontent.com/cP82wBv04ib5r_7V_8lAgCXLhHTMztEKcDz6UWHHw3QuuPVKDsW68VesTgyYxSEVx9Fnb01PoFl4iD2RPQ=w544-h544-l90-rj`,
+    "artists": ["nonoc"],
+    "album": "TV Re: Zero kara Hajimeru Isekai Seikatsu 2nd Season",
+    "duration": "5:05"
   },
   {
     "id": 2,
@@ -231,11 +231,11 @@ export const songs: Song[] = [
   {
     "id": 1,
     "albumId": 4,
-    "title": "Lunar",
-    "image": "https://f4.bcbits.com/img/a1962013209_16.jpg",
-    "artists": ["Tenno"],
-    "album": "Study Session",
-    "duration": "3:40"
+    "title": "Absolute Soul",
+    "image": "https://lh3.googleusercontent.com/WSUuoJZ2CwEkT7udiZzZyhGBe2jbiU2QKq894Nc7ZkhDTv0KL1odxJPeuRsFSS6Wi1BP7kWOTYLFOdSDxA=w544-h544-s-l90-rj",
+    "artists": ["Konomi Suzuki"],
+    "album": "Absolute Soul",
+    "duration": "4:43"
   },
   {
     "id": 2,
